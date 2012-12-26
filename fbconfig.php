786fd025ed78fad070304470fd0c7ed15fcd2f0e
induri2k7 <induri2k7@gmail.com>
2012-12-26 04:04:58 -0800

    Create fbconfig.php

---
+++ fbconfig.php
@@ -0,0 +1,8 @@
+<?php
+$facebook_appid='242494765877295';
+$facebook_app_secret='60445fb3c27ab2f43211537d3ff0200f';
+$facebook = new Facebook(array(
+'appId' => $facebook_appid,
+'secret' => $facebook_app_secret,
+));
+?>
