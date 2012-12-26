b2133635b9d7989729a09c2818900fa7e4d4a0a2
induri2k7 <induri2k7@gmail.com>
2012-12-26 17:35:21 +0530

    Update example.php

--- example.php
+++ example.php
@@ -15,7 +15,7 @@
 * under the License.
 */
 
-require 'lib/facebook.php';
+require 'facebook.php';
 
 // Create our Application instance (replace this with your appId and secret).
 $facebook = new Facebook(array(
